4a1fd81b00216d7b38de3c69ee054746880ff3f5
Benaouali Mokhtaria <benaoualimokhtaria@hotmail.com>
2021-12-16 17:07:48 +0100

    Inteface cration

---
+++ appJs.js
@@ -0,0 +1,8 @@
+const video = document.querySelector(".video");
+const playButton = document.querySelector(".playButton");
+const volume = document.querySelector(".volume");
+const progress = document.querySelector(".progress");
+const backwordButton = document.querySelector(".backwordButton");
+const forwordButton = document.querySelector(".forwordButton");
+
+/* Functio */
