735648238da6125d91b3d215735bcf0953752ec5
Benaouali Mokhtaria <benaoualimokhtaria@hotmail.com>
2021-12-17 18:41:53 +0100

    Inteface cration

--- appJs.js
+++ appJs.js
@@ -3,6 +3,42 @@ const playButton = document.querySelector(".playButton");
 const volume = document.querySelector(".volume");
 const progress = document.querySelector(".progress");
 const backwordButton = document.querySelector(".backwordButton");
-const forwordButton = document.querySelector(".forwordButton");
+const forwardButton = document.querySelector(".forwardButton");
 
-/* Functio */
+
+//function progress bar
+function progressBarUpdate(){
+    progress.style.flexBasis = (video.currentTime/video.duration) * 100 + '%';
+}
+/* Function play video */
+   function playVideo(){
+    if(video.paused){
+        video.play();
+        playButton.firstChild.classList.remove('fa-play');
+        playButton.firstChild.classList.add('fa-pause')
+    }
+    else{
+        video.pause();
+        playButton.firstChild.classList.remove('fa-pause');
+        playButton.firstChild.classList.add('fa-play');   
+    }  
+    }
+video.addEventListener('click', playVideo);
+playButton.addEventListener('click', playVideo);
+
+
+
+video.addEventListener('timeupdate', progressBarUpdate);
+
+
+volume.addEventListener('change', () => {video.volume = volume.value});
+
+backwordButton.addEventListener('click', () =>{
+        video.currentTime -=1;
+        progressBarUpdate();
+ });
+forwardButton.addEventListener('click', () => {
+        video.currentTime +=1;
+        progressBarUpdate();
+});
+                               
